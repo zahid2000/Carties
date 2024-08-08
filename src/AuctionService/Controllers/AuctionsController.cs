8ed5307a6e4bc8b2752b750dadeedac5a4f6bee7
zahid2000 <zahidmamedov07@gmail.com>
2024-08-08 17:09:57 +0400

    Auction service api completed

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,87 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+                .Include(x => x.Item)
+                .OrderBy(x => x.Item.Make)
+                .ToListAsync();
+        var mappedAuctions = _mapper.Map<List<AuctionDto>>(auctions);
+        return mappedAuctions;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+                .Where(x => x.Id == id)
+                .Include(x => x.Item)
+                .FirstOrDefaultAsync();
+        if (auction == null) return NotFound();
+
+        var mappedAuction = _mapper.Map<AuctionDto>(auction);
+        return mappedAuction;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        Auction auction = _mapper.Map<Auction>(auctionDto);
+        //TODO: add current user to seller
+        auction.Seller = "test";
+
+        await _context.Auctions.AddAsync(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result)
+            return BadRequest("Could not save changes to the DB");
+
+        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto auctionDto)
+    {
+        Auction auction = await _context.Auctions.Where(x => x.Id == id).Include(x => x.Item).FirstOrDefaultAsync();
+        if (auction == null) return NotFound();
+        //TODO: check seller == current username
+        var mappedAuction = _mapper.Map(auctionDto, auction);
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result) return BadRequest("Problem sacing changes");
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+        if (auction == null) return NotFound();
+        //TODO: check seller == current username
+        _context.Auctions.Remove(auction);
+        var result = await _context.SaveChangesAsync() > 0;
+        if (!result) return BadRequest("Could not update DB");
+        return Ok(result);
+    }
+}
+
---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>().ForMember(d => d.Item, opt => opt.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+        CreateMap<UpdateAuctionDto, Auction>().ForMember(d => d.Item, opt => opt.MapFrom(s => s));
+        CreateMap<UpdateAuctionDto, Item>();
+    }
+}
